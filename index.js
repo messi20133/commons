79fd929697e7b71f3fbd2a0d2af156613e58b733
tangyunlong02 <tangyunlong02@meituan.com>
2021-06-15 10:19:06 +0800

    feat: save

---
+++ index.js
@@ -0,0 +1,34 @@
+import Text1 from './text.vue'
+// console.log('======')
+// console.log(Text)
+// module.exports = {
+//   add: function(a, b) {
+//     return a + b
+//   },
+//   minus: function(a, b) {
+//     return a - b
+//   },
+
+//   // Text
+//   // Text: '11111'
+// }
+
+// export default {
+//     add: function(a, b) {
+//     return a + b
+//   },
+//   minus: function(a, b) {
+//     return a - b
+//   },
+
+//   Text
+// }
+const add = function(a, b) {
+  return a + b
+}
+const minus = function(a, b) {
+  return a  - b
+}
+export {
+  add,minus, Text1
+}
---
+++ text.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>hello, {{msg}}</h1>
+</template>
+<script>
+export default {
+  name: 'Text1',
+  props: {
+    msg: String
+  }
+}
+</script>
