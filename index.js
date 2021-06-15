6e2e79cc9a688e2ec56d6cebb9f04cc12f0823c1
tangyunlong02 <tangyunlong02@meituan.com>
2021-06-15 16:52:14 +0800

    feat: add test

--- index.js
+++ index.js
@@ -1,33 +1,9 @@
 import Text1 from './text.vue'
-// console.log('======')
-// console.log(Text)
-// module.exports = {
-//   add: function(a, b) {
-//     return a + b
-//   },
-//   minus: function(a, b) {
-//     return a - b
-//   },
-
-//   // Text
-//   // Text: '11111'
-// }
-
-// export default {
-//     add: function(a, b) {
-//     return a + b
-//   },
-//   minus: function(a, b) {
-//     return a - b
-//   },
-
-//   Text
-// }
 const add = function(a, b) {
-  return a + b
+  return a + b + 9;
 }
 const minus = function(a, b) {
-  return a  - b
+  return a  - b - 1;
 }
 export {
   add,minus, Text1
